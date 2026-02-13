57482f390da0b15e029fc2bcd88177bb62912593
Sandeep Kumar Gupta <sangupta019@gmail.com>
2026-02-13 17:10:34 +0530

    Complete Working Reflection Agent

--- reflection-agent/main.py
+++ reflection-agent/main.py
@@ -14,14 +14,16 @@ REFLECT = "reflect"
 GENERATE = "generate"
 
 # 1. Define Generation Node -> It recieve the State history
-def generation_node(state: Sequence[BaseMessage]):
-    return generation_chain.invoke({"messages": state})
+def generation_node(state: GraphState):
+    res = generation_chain.invoke({"messages": state["messages"]})
+    # because state is a Dict we need to retrun it this way
+    return {"messages": state["messages"] + [res]}
 
 # 2. Define Reflection node -> it receievs the history in messages a salso from above
-def reflection_node(messages: Sequence[BaseMessage]) -> List[BaseMessage]:
-    res = reflection_chain.invoke({"messages": messages})
+def reflection_node(state: GraphState):
+    res = reflection_chain.invoke({"messages": state["messages"]})
     # store it asd we will trick AI by making it feel like human message , whih it has to refine
-    return [HumanMessage(content=res.content)]
+    return {"messages": state["messages"] + [HumanMessage(content=res.content)]}
     
 # 3. Initialize graph and add nodes
 builder = StateGraph(state_schema=GraphState)
@@ -30,7 +32,7 @@ builder.add_node(REFLECT, reflection_node)
 builder.set_entry_point(GENERATE)
 
 # 4. write sould continue node 
-def should_continue(state: List[BaseMessage]):
+def should_continue(state: GraphState):
     if len(state) > 6:
         return END 
     else:
@@ -51,5 +53,21 @@ open("graph.png", "wb").write(graph.get_graph().draw_mermaid_png())
 
 
 if __name__ == "__main__":
-    pass
+    # 8. lets run the functionality
+    print("Hello Langgraph User")
+
+    inputs = HumanMessage(content="""Make this tweet better:"
+                          At Seva Teerth earlier today, signed files that are connected with the empowerment of the poor, downtrodden, our hardworking farmers, Yuva Shakti and Nari Shakti. The key decisions include:
+        Approval for the launch of PM RAHAT scheme, as per which victims of accidents will get cashless treatment of up to Rs. 1.5 lakhs. This will ensure that immediate medical help is provided to anyone affected. 
+        Doubling of the target of the Lakhpati Didi scheme to 6 crore. This will boost women empowerment and self-reliance. 
+        The Agriculture Infrastructure Fund target has been doubled from Rs. 1 lakh crore to Rs. 2 lakh crore. This will be immensely helpful to our farmers and help boost their incomes.
+        Approval for a Startup India Fund of Funds 2.0 with a corpus of Rs. 10,000 crore. This will encourage startups in early stages and deep-tech research.
+        These decisions reaffirm the spirit of Nagrikdevo Bhava and add momentum to our vision of building a Viksit Bharat.""")
+    
+    response = graph.invoke(
+        {
+            "messages": [inputs]
+        }
+    )
+
 
